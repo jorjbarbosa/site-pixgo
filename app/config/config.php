843e23a7a8b3d63ae7e1cb4c42590c28ae521ca9
Jorge Barbosa <jorgebarbosa_@outlook.com>
2019-04-22 15:06:30 -0400

    Reset

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '');
+  define('DB_NAME', 'pixgo_db');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/site-pixgo');
+  // Site Name
+  define('SITENAME', 'Laboratório PixGo');

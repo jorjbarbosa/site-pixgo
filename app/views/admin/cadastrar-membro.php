fd600749799cb984ce6a7e9af71f43a933094681
Jorge Barbosa <jorgebarbosa_@outlook.com>
2019-05-27 16:14:32 -0400

    alteração nos formulários de cadastro

--- app/views/admin/cadastrar-membro.php
+++ app/views/admin/cadastrar-membro.php
@@ -5,27 +5,27 @@
         <form action="<?php echo URLROOT.'/admin/cadastrar_membro/'?>" method="post" enctype="multipart/form-data">
           <div class="form-group">
             <label for="nome">Nome</label>
-            <input type="text" class="form-control" name="nome"><br>
+            <input type="text" class="form-control" name="nome" required><br>
           </div>
           <div class="form-group">
             <label for="area">Área de Pesquisa</label>
-            <input type="text" class="form-control" name="area_pesquisa"><br>
+            <input type="text" class="form-control" name="area_pesquisa" required><br>
           </div>
           <div class="form-group">
             <label for="facebook">Facebook</label>
-            <input type="text" class="form-control" name="facebook"><br>
+            <input type="text" class="form-control" name="facebook" required><br>
           </div>
           <div class="form-group">
             <label for="linkedin">Linkedin</label>
-            <input type="text" class="form-control" name="linkedin"><br>
+            <input type="text" class="form-control" name="linkedin" required><br>
           </div>
           <div class="form-group">
             <label for="github">Github</label>
-            <input type="text" class="form-control" name="github"><br>
+            <input type="text" class="form-control" name="github" required><br>
           </div>
           <div class="form-group">
             <label for="foto">Avatar</label>
-            <input type="file" class="form-control-file" name="foto" ><br>
+            <input type="file" class="form-control-file" name="foto"  required><br>
           </div>
           <input type="submit" value="Enviar" class="btn btn-pixgo btn-block">
         </form>
--- app/views/admin/cadastrar-publicacao.php
+++ app/views/admin/cadastrar-publicacao.php
@@ -5,27 +5,27 @@
       <form action="<?php echo URLROOT.'/admin/cadastrar_publicacao/'?>" method="post" enctype="multipart/form-data">
         <div class="form-group">
           <label for="titulo">Titulo</label>
-          <input type="text" class="form-control" name="titulo">
+          <input type="text" class="form-control" name="titulo" required>
         </div>
         <div class="form-group">
           <label for="autores">Autores</label>
-          <input type="text" name="autores" class="form-control">
+          <input type="text" name="autores" class="form-control" required>
         </div>
         <div class="form-group">
           <label for="conferencia">Conferencia</label>
-          <input type="text" name="conferencia" class="form-control">
+          <input type="text" name="conferencia" class="form-control" required>
         </div>
         <div class="form-group">
           <label for="ano">Ano</label>
-          <input type="text" name="ano" class="form-control">
+          <input type="text" name="ano" class="form-control" required>
         </div>
         <div class="form-group">
           <label for="url">URL</label>
-          <input type="text" name="url" class="form-control">
+          <input type="text" name="url" class="form-control" required>
         </div>
         <div class="form-group">
           <label for="resumo">Resumo</label>
-          <textarea name="resumo"cols="30" rows="10" class="form-control"></textarea>
+          <textarea name="resumo"cols="30" rows="10" class="form-control" required></textarea>
         </div>
        
         <input type="submit" value="Enviar" class="btn btn-pixgo btn-block">
